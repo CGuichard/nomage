f36161ac3c52c87a31575c72d792807934262e5d
Clément GUICHARD <clement.guichard0@gmail.com>
2025-05-03 22:19:22 +0200

    feat: move CLI impl from click to argparse, simplify to single-command

--- src/nomage/_cli.py
+++ src/nomage/_cli.py
@@ -1,21 +1,30 @@
 """Command-line interface."""
 
+import argparse
+import sys
 from importlib.metadata import metadata
 
-import click
+
+def main(_args: list[str] | None = None, /) -> None:
+    if _args is None:
+        _args = sys.argv[1:]
+
+    parser = create_parser()
+    args = parser.parse_args(_args)
+
+    if args.version:
+        pkg_metadata = metadata(__package__)
+        print(pkg_metadata["Version"])
+
+    sys.exit(0)
 
 
-@click.group()
-def main() -> None:
-    """Nomage CLI."""
-
-
-@main.command()
-def version() -> None:
-    """Print version and exit."""
-    m = metadata(__package__)
-    pkg_name = m["Name"]
-    pkg_version = m["Version"]
-    pkg_summary = m["Summary"]
-    print(f"{pkg_name}: {pkg_version}")
-    print(pkg_summary)
+def create_parser() -> argparse.ArgumentParser:
+    pkg_metadata = metadata(__package__)
+    parser = argparse.ArgumentParser(
+        description=f"{pkg_metadata['Name']} - {pkg_metadata['Summary']}",
+    )
+    parser.add_argument(
+        "-V", "--version", action="store_true", help="print version and exit"
+    )
+    return parser
